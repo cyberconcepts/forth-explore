4ed426f8e87e3af692cd8f656c6e26a83d1f60a4
Helmut Merz <helmutm@cy55.de>
2018-06-27 12:14:46 +0200

    rdf stuff: work in progress...

--- rdf.fs
+++ rdf.fs
@@ -1,11 +1,12 @@
 
 
 : node ( "name" -- addr )
-    ( todo: check if name exists and is a node )
-    ( todo: store namespace instead of 0 )
-    parse-name here dup { addr }
-    0 , 0 , $!
-    addr dup $@ nextname create here swap , ;
+    \ todo: check if name exists (use 'defined') - and is a node?
+    \ todo: store namespace instead of 0
+    here { namep }  \ remember name address
+    0 , 0 ,         \ alloc and initialize node
+    parse-name 2dup namep $!        \ get name, store name address in node
+    nextname create here namep , ;  \ create and set variable, keep address
     \ does> ;
 
 : getname ( addr1 -- addr2 u )
